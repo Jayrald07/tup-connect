5399361bcde75188b9e93c793e826238e132e2d9
Jayrald07 <jayrald.empino@tup.edu.ph>
2021-11-12 15:09:45 +0800

    add post creation and deletion

---
+++ application/controllers/Forum.php
@@ -0,0 +1,59 @@
+<?php
+class Forum extends CI_Controller
+{
+
+    private $mock_session_data = array('user_id' => 'u123');
+
+    public function __construct()
+    {
+        parent::__construct();
+        $this->load->library(array('post', 'session'));
+    }
+
+    public function index()
+    {
+        $this->session->set_userdata($this->mock_session_data);
+        $data["type"] = "forum";
+        $data["posts"] = $this->post->get_posts('forum');
+
+        $this->load->view("view_post", $data);
+    }
+
+    public function create()
+    {
+
+        $this->post->create_form(array(
+            "group" => false,
+            "campus" => false,
+            "college" => false,
+            "category" => true,
+            "type" => "forum"
+        ));
+    }
+
+    public function submit()
+    {
+        $date = new DateTime('now');
+        $post_id = random_string('alnum', 15);
+
+        $data = array(
+            "allowed" => "*",
+            "type" => 'forum',
+            "certain" => array(
+                "forum_id" => random_string('alnum', 15),
+                "user_detail_id" => $this->session->userdata('user_id'),
+                "category_id" => $this->input->post("category"),
+                "post_id" => $post_id,
+            ),
+            "post_id" => $post_id,
+            "post_text" => $this->input->post('content'),
+            "post_up_vote" => 0,
+            "post_down_vote" => 0,
+            "date_time_stamp" => $date->format("Y-m-d H:i:s"),
+            "status" => "posted",
+        );
+
+        if ($this->post->submit($data)) redirect("./forum");
+        else redirect("./forum/create");
+    }
+}
---
+++ application/models/Post_model.php
@@ -0,0 +1,107 @@
+<?php
+
+class Post_model extends CI_Model
+{
+
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    public function get_table_per_type($type)
+    {
+        if ($type == 'lobby') return 'tbl_lobby';
+        if ($type == 'fw') return 'tbl_freedom_wall';
+        if ($type == 'forum') return 'tbl_forum';
+    }
+
+    public function set_new_type($data, $type)
+    {
+        return $this->db->insert($this->get_table_per_type($type), $data);
+    }
+
+    public function set_new_post($data)
+    {
+        return $this->db->insert("tbl_post", $data);
+    }
+
+    public function set_new_lobby_post($data)
+    {
+        return $this->db->insert("tbl_lobby_post", $data);
+    }
+
+    public function set_new_post_image($post_image_id, $post_id)
+    {
+        $data = array(
+            "post_image_id" => $post_image_id,
+            "post_id" => $post_id,
+            "post_image_path" => $post_image_id
+        );
+        return $this->db->insert("tbl_post_image", $data);
+    }
+
+    public function get_groups()
+    {
+        $query = $this->db->get("tbl_group");
+        return $query->result_array();
+    }
+
+    public function get_campuses()
+    {
+        $query = $this->db->get("tbl_campus");
+        return $query->result_array();
+    }
+
+    public function get_colleges()
+    {
+        $query = $this->db->get("tbl_college");
+        return $query->result_array();
+    }
+
+    public function get_categories()
+    {
+        $query = $this->db->get("tbl_category");
+        return $query->result_array();
+    }
+
+    public function remove_post($post_id)
+    {
+        $this->db->set("status", "removed");
+        $this->db->where("post_id", $post_id);
+        $this->db->update("tbl_post");
+        return true;
+    }
+
+    public function get_posts($type)
+    {
+        switch ($type) {
+            case 'lobby':
+                $query = $this->db->query("
+                select tbl_lobby_post.category_id, tbl_post.*
+                from tbl_lobby, tbl_lobby_post, tbl_post
+                where 
+                    tbl_lobby.lobby_id = tbl_lobby_post.lobby_id and tbl_lobby_post.post_id = tbl_post.post_id and tbl_post.status = 'posted'
+                    ORDER BY tbl_post.date_time_stamp desc");
+                return $query->result_array();
+                break;
+
+            case 'fw':
+                $query = $this->db->query("
+                select tbl_freedom_wall.fw_id, tbl_post.*
+                from tbl_freedom_wall, tbl_post
+                where tbl_freedom_wall.post_id = tbl_post.post_id and tbl_post.status = 'posted'
+                order by tbl_post.date_time_stamp desc");
+                return $query->result_array();
+                break;
+
+            case 'forum':
+                $query = $this->db->query("
+                select tbl_forum.forum_id, tbl_forum.category_id, tbl_post.*
+                from tbl_forum, tbl_post
+                where tbl_forum.post_id = tbl_post.post_id and tbl_post.status = 'posted'
+                order by tbl_post.date_time_stamp desc");
+                return $query->result_array();
+                break;
+        }
+    }
+}
