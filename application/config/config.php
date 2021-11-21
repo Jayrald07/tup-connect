917bf841efe41a1874263900441598492f73885a
Jay <jayrald.empino@tup.edu.ph>
2021-11-21 14:41:48 +0800

    update base_url

--- application/config/config.php
+++ application/config/config.php
@@ -24,7 +24,7 @@ defined('BASEPATH') or exit('No direct script access allowed');
 |
 */
 
-$config['base_url'] = 'http://localhost';
+$config['base_url'] = 'http://localhost/tup-connect';
 
 /*
 |--------------------------------------------------------------------------
