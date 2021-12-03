413233b778afc70e0bb17f8deffdc9436044e8c1
Jay <jayrald.empino@tup.edu.ph>
2021-12-03 10:55:14 +0800

    fix sql file and password management

--- application/controllers/Login.php
+++ application/controllers/Login.php
@@ -1,15 +1,12 @@
 <?php
 
-require("application/third_party/PHPMailer/PHPMailer.php");
-require("application/third_party/PHPMailer/SMTP.php");
-
 class Login extends CI_Controller
 {
     public function __construct()
     {
         parent::__construct();
         $this->load->helper(array("form", "url"));
-        $this->load->library('form_validation');
+        $this->load->library(array('form_validation', 'email'));
         $this->load->library('session');
     }
 
@@ -19,114 +16,82 @@ class Login extends CI_Controller
     }
 
     public function index()
-	{
-			$this->load->view('login');
-	
-	}
+    {
+        $this->load->view('login');
+    }
 
     public function forgotPassword()
     {
         $this->load->model("login_model");
-        if($_SERVER['REQUEST_METHOD']=='POST')
-        {
-            $this->form_validation->set_rules('email','Email','required');
-            if($this->form_validation->run()==TRUE)
-            {
+        if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+            $this->form_validation->set_rules('email', 'Email', 'required');
+            if ($this->form_validation->run() == TRUE) {
                 $email = $this->input->post('email');
                 $validateEmail = $this->login_model->validateEmail($email);
-                if($validateEmail!=false)
-                {
-                    $row= $validateEmail;
+                if ($validateEmail != false) {
+                    $row = $validateEmail;
                     $user_id = $row->id;
-                    
-                    $string = time().$user_id.$email;
-                    $hash_string = hash('sha256',$string);
+
+                    $string = time() . $user_id . $email;
+                    $hash_string = hash('sha256', $string);
                     $currentDate = date('Y-m-d H:i');
-                    $hash_expiry = date('Y-m-d H:I', strtotime($currentDate. '+1 days'));
+                    $hash_expiry = date('Y-m-d H:I', strtotime($currentDate . '+1 days'));
                     $data = array(
-                        'hash_key' =>$hash_string, 
-                        'hash_expiry'=>$hash_expiry,
+                        'hash_key' => $hash_string,
+                        'hash_expiry' => $hash_expiry,
                     );
 
 
-                    $resetLink = base_url().'index.php/reset/password?hash='.$hash_string;
-                    $message = '<p>Click your verification link here:</p>'.$resetLink;
+                    $resetLink = base_url() . 'index.php/reset/password?hash=' . $hash_string;
+                    $message = '<p>Click your verification link here:</p>' . $resetLink;
                     $subject = "Password Reset Link";
-                    $sentStatus = $this->sendEmail($email,$subject,$message);
-                    
-                    if($sentStatus==true)
-                    {
-                        $this->login_model->updatePasswordhash($data,$email);
+                    $sentStatus = $this->sendEmail($email, $subject, $message);
+
+                    if ($sentStatus == true) {
+                        $this->login_model->updatePasswordhash($data, $email);
                         redirect(base_url('index.php/login/forgotPassword'));
-                        echo'hehe';
-                    }
-                    else{
-                        $this->session->set_flashdata('error','Email sending error');
+                        echo 'hehe';
+                    } else {
+                        $this->session->set_flashdata('error', 'Email sending error');
                         $this->load->view('forgotPass');
-                        echo'hotdog';
+                        echo 'hotdog';
                     }
-                }
-                else{
-                    $this->session->set_flashdata('error','invalid email id');
+                } else {
+                    $this->session->set_flashdata('error', 'invalid email id');
                     $this->load->view('forgotPass');
                     echo 'wew';
                 }
-            }
-            else
-            {
+            } else {
                 $this->load->view('forgotPass');
                 echo 'ha';
             }
-        }
-        else
-        {
+        } else {
             $this->load->view('forgotPass');
             echo 'heh';
         }
-
     }
 
-	public function sendEmail($email,$subject,$message)
+    public function sendEmail($email, $subject, $message)
     {
-        $mailTo = $email;
-        $body = $message;
-        
-        $mail = new PHPMailer\PHPMailer\PHPMailer();
+        $config = array(
+            'protocol' => 'smtp',
+            'smtp_host' => 'smtp-relay.sendinblue.com',
+            'smtp_port' => 587,
+            'smtp_user' => 'hyakki03@gmail.com',
+            'smtp_pass' => 'fWKzMChYJ8nNsDv5',
+            'mailtype' => 'html',
+            'charset' => 'iso-8859-1'
+        );
+        $this->email->initialize($config);
+        $this->email->set_mailtype("html");
+        $this->email->set_newline("\r\n");
 
-        $mail->SMTPDebug = 3;
+        $this->email->to($email);
+        $this->email->from('admin@hyakki.com', 'TUP Connect');
+        $this->email->subject('Password Reset Link');
+        $this->email->message($message);
 
-        $mail->isSMTP();
-
-        $mail->Host = "smtp-relay.sendinblue.com";
-
-        $mail->SMTPAuth = true;
-        
-        $mail->Username = "hyakki03@gmail.com";
-        $mail->Password = "fWKzMChYJ8nNsDv5";
-
-        $mail->SMTPSecure = "tls";
-
-        $mail->Port = "587";
-
-        $mail->From = "admin@hyakki.com";
-        $mail->FromName = "Hotdog";
-
-        $mail->addAddress($mailTo, "Test");
-
-        $mail->isHTML(true);
-
-        $mail->Subject = $subject;
-        $mail->Body = $message;
-        $mail->AltBody = "ratatatatat";
-
-        if(!$mail->send()){
-            return false;
-        }
-        else{
-            return true;
-        }
+        if ($this->email->send()) return true;
+        else return false;
     }
 }
-
-
-?>
--- application/models/login_model.php
+++ application/models/login_model.php
@@ -3,103 +3,85 @@
 class login_model extends CI_Model
 
 {
-    public function __construct()
-    {
-        parent::__construct();
-    }
-
-    public function authenticate()
-    {
-        $query = $this->db->query("SELECT user_detail_id from tbl_user where (user_name ='".$this->input->post("username")."' or user_email = '".$this->input->post("username")."') and user_password = '".$this->input->post("password")."'");
-
-        return $query->result_array();
-    }
-
-
-    function checkUser($email,$password)
+	public function __construct()
 	{
-		$query = $this->db->query("SELECT * from users where email='$email' AND password='$password'");
-		if($query->num_rows()==1)
-		{
+		parent::__construct();
+	}
+
+	public function authenticate()
+	{
+		$query = $this->db->query("SELECT user_detail_id from tbl_user where (user_name ='" . $this->input->post("username") . "' or user_email = '" . $this->input->post("username") . "') and user_password = '" . $this->input->post("password") . "'");
+
+		return $query->result_array();
+	}
+
+
+	function checkUser($email, $password)
+	{
+		$query = $this->db->query("SELECT * from tbl_pass_manage where email='$email' AND password='$password'");
+		if ($query->num_rows() == 1) {
 			return $query->row();
-		}
-		else
-		{
+		} else {
 			return false;
 		}
 	}
 
-    function checkCurrentPassword($currentPassword)
+	function checkCurrentPassword($currentPassword)
 	{
 		$userid = $this->session->userdata('LoginSession')['id'];
-		$query = $this->db->query("SELECT * from users WHERE id='$userid' AND password='$currentPassword' ");
-		if($query->num_rows()==1)
-		{
+		$query = $this->db->query("SELECT * from tbl_pass_manage WHERE id='$userid' AND password='$currentPassword' ");
+		if ($query->num_rows() == 1) {
 			return true;
-		}
-		else
-		{
+		} else {
 			return false;
 		}
 	}
 
-    function updatePassword($password)
+	function updatePassword($password)
 	{
 		$userid = $this->session->userdata('LoginSession')['id'];
-		$query = $this->db->query("UPDATE users set password='$password' WHERE id='$userid' ");
-		
+		$query = $this->db->query("UPDATE tbl_pass_manage set password='$password' WHERE id='$userid' ");
 	}
 
-    public function validateEmail($email)
-    {
-        $query = $this->db->query("SELECT * FROM users WHERE email='$email'");
-        if($query->num_rows() == 1)
-        {
-            return $query->row();
-        }
-        else
-        {
-            return false;
-        }
-    }
-
-    public function updatePasswordhash($data,$email)
-    {
-        $this->db->where('email',$email);
-        $this->db->update('users',$data);
-    }
-
-    function getHashDetails($hash)
+	public function validateEmail($email)
 	{
-		$query =$this->db->query("SELECT * from users WHERE hash_key='$hash'");
-		if($query->num_rows()==1)
-		{
+		$query = $this->db->query("SELECT * FROM tbl_pass_manage WHERE email='$email'");
+		if ($query->num_rows() == 1) {
 			return $query->row();
-		}
-		else
-		{
+		} else {
 			return false;
 		}
-
 	}
 
-    function validateCurrentPassword($currentPassword,$hash)
+	public function updatePasswordhash($data, $email)
 	{
-		$query = $this->db->query("SELECT * from users WHERE password = '$currentPassword' AND hash_key='$hash'");
-		if($query->num_rows()==1)
-		{
+		$this->db->where('email', $email);
+		$this->db->update('tbl_pass_manage', $data);
+	}
+
+	function getHashDetails($hash)
+	{
+		$query = $this->db->query("SELECT * from tbl_pass_manage WHERE hash_key='$hash'");
+		if ($query->num_rows() == 1) {
+			return $query->row();
+		} else {
+			return false;
+		}
+	}
+
+	function validateCurrentPassword($currentPassword, $hash)
+	{
+		$query = $this->db->query("SELECT * from tbl_pass_manage WHERE password = '$currentPassword' AND hash_key='$hash'");
+		if ($query->num_rows() == 1) {
 			return true;
-		}
-		else
-		{
+		} else {
 			return false;
 		}
 	}
 
-    function updateNewPassword($data,$hash)
+	function updateNewPassword($data, $hash)
 	{
-		$this->db->where('hash_key',$hash);
-		$this->db->update('users',$data);
+		$this->db->where('hash_key', $hash);
+		$this->db->update('tbl_pass_manage', $data);
 	}
 }
-?>
